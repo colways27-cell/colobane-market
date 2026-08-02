821daeea571d19b6fdb499b5a769a06f32ab233c
sgglobalbuissness-sys <sgglobalbuissness@gmail.com>
2026-08-02 14:44:23 +0000

    fix: selfDestroying service worker to wipe client SW caches

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      selfDestroying: true,
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
